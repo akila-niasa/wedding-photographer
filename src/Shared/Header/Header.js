b8f99b3665342f894cdcf7afa3966a5a08b98fc0
akila-niasa <akilaniasa@gmail.com>
2022-04-16 20:51:07 +0600

    first commit

---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" className='nav' variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Independent Photography</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mx-auto">
+    <Link to='/' className='text-decoration-none text-light p-2 m-2'>Home</Link>
+        <Link to='/interview'className='text-decoration-none text-light p-2 m-2'>About Me</Link>
+        <Link to='/login'className='text-decoration-none text-light p-2 m-2'>LOgin</Link>
+    </Nav>
+    
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    );
+};
+
+export default Header;
