7cc89784cb22f9027a74105d2533f16d4df3ac74
akila-niasa <akilaniasa@gmail.com>
2022-04-17 12:19:33 +0600

    successfully login with firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css'
 
 const Login = () => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
-    const [confirmPassword,setConfirmPassword]=useState('')
+  
     const[error,setError]=useState('')
     const navigate=useNavigate()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error1,
+      ] = useSignInWithEmailAndPassword(auth);
   
     const handleEmail=event=>{
         setEmail(event.target.value);
@@ -17,10 +25,17 @@ const Login = () => {
     const handlePassword=event=>{
         setPassword(event.target.value)
     }
+    const handleSubmit=event=>{
+        event.preventDefault()
+        signInWithEmailAndPassword(email,password)
+    }
+    if(user){
+        navigate('/checkout')
+    }
     return (
         <div className='container card w-50 mx-auto mt-5'>
         <h2 className='text-primary fw-bold text-center mt-4 mb-3'>Please Login</h2>
-        <Form >
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control onBlur={handleEmail}  type="email" placeholder="Enter email" required />
